639802171d44df22340d6df9d88a2a31bf093bf7
Musgame1 <59405531+MusMansouri@users.noreply.github.com>
2024-08-28 14:44:39 +0200

    modification

--- brocante/src/component/NavBar.vue
+++ brocante/src/component/NavBar.vue
@@ -1,65 +1,61 @@
-<script setup ></script>
-<template>   
- <nav class="navbar">
+<script setup></script>
+<template>
+  <nav class="navbar">
     <div class="navbar-container">
-        <a href="#" class="brand-logo">La Brocante</a>
-        <ul class="nav-links">
-            <li><a href="#home">Accueil</a></li>
-            <li><a href="#categories">Catégories</a></li>
-            <li><a href="#about">À propos</a></li>
-        </ul>
-
+      <a href="#" class="brand-logo">La Brocante</a>
+      <ul class="nav-links">
+        <li><router-link to="./">Accueil</router-link>/li></li>
+        <li><a href="#categories">Catégories</a></li>
+        <li><a href="#about">À propos</a></li>
+      </ul>
     </div>
-</nav>
+  </nav>
 </template>
 <style scoped>
-
 /* Styles pour la Navbar */
 .navbar {
-    background-color: #333;
-    padding: 10px 20px;
-    position: fixed;
-    width: 100%;
-    top: 0;
-    left: 0;
-    z-index: 1000;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  background-color: #333;
+  padding: 10px 20px;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
 
 .navbar-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-right: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-right: 50px;
 }
 
 .brand-logo {
-    color: white;
-    font-size: 24px;
-    text-decoration: none;
+  color: white;
+  font-size: 24px;
+  text-decoration: none;
 }
 
 .nav-links {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
 }
 
 .nav-links li {
-    margin-left: 24px;
+  margin-left: 24px;
 }
 
 .nav-links a {
-    color: white;
-    text-decoration: none;
-    font-size: 16px;
-    transition: color 0.3s;
+  color: white;
+  text-decoration: none;
+  font-size: 16px;
+  transition: color 0.3s;
 }
 
 .nav-links a:hover {
-    color: #007bff;
+  color: #007bff;
 }
-
-
 </style>
