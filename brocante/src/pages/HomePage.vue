4e8fe38305c562bd9d6807392e0d2f224fd61e9d
Musgame1 <59405531+MusMansouri@users.noreply.github.com>
2024-08-29 10:01:47 +0200

    mise a jour

--- brocante/src/pages/HomePage.vue
+++ brocante/src/pages/HomePage.vue
@@ -1,97 +1,5 @@
 <script setup>
 import CardComponent from "@/component/CardComponent.vue";
-import { ref } from "vue";
-
-const produits = ref([
-  {
-    id: "1",
-    title: "vente de violon",
-    description: "blablablabla",
-    price: 750,
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Old_violin.jpg/280px-Old_violin.jpg",
-    userId: "1",
-  },
-  {
-    id: "2",
-    title: "guitaar",
-    description: "blablabla",
-    price: 1500,
-    imageUrl:
-      "https://www.midimusic.fr/1310-large_default/epiphone-guitare-electrique-sg-standard-cherry-gaucher.jpg",
-    userId: "1",
-  },
-  {
-    id: "3",
-    title: "tambour",
-    description: "zaeaeaeaze",
-    price: 500,
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/e/eb/M0354_1951-15-077_1.jpg",
-    userId: "1",
-  },
-  {
-    id: "4",
-    title: "piano",
-    description: "dfdsfhdslfkdlkfjsdf",
-    price: 250000,
-    imageUrl: "https://www.musique-galland.fr/medias/images/c3xa.jpg",
-    userId: "1",
-  },
-  {
-    id: 5,
-    title: "test",
-    description: "test",
-    price: 55,
-    imageUrl:
-      "https://emisb.sisteronais-buech.fr/wp-content/uploads/sites/2/2019/08/Copie-de-contenus-types11.png",
-    userId: "1",
-  },
-  {
-    id: 6,
-    title: "test",
-    description: "test",
-    price: 555,
-    imageUrl:
-      "https://www.jeuxetcompagnie.fr/wp-content/uploads/2014/01/coloriage-instrument-de-musique.jpg",
-    userId: "1",
-  },
-  {
-    id: 7,
-    title: "test",
-    description: "test",
-    price: 555,
-    imageUrl:
-      "https://www.jeuxetcompagnie.fr/wp-content/uploads/2014/01/coloriage-instrument-de-musique.jpg",
-    userId: "1",
-  },
-  {
-    id: 8,
-    title: "test",
-    description: "test",
-    price: 555,
-    imageUrl:
-      "https://www.jeuxetcompagnie.fr/wp-content/uploads/2014/01/coloriage-instrument-de-musique.jpg",
-    userId: "1",
-  },
-  {
-    id: 9,
-    title: "test",
-    description: "test",
-    price: 555,
-    imageUrl:
-      "https://www.jeuxetcompagnie.fr/wp-content/uploads/2014/01/coloriage-instrument-de-musique.jpg",
-    userId: "1",
-  },
-  {
-    id: 10,
-    title: "flute",
-    description: "flute blablalba",
-    price: 360,
-    imageUrl: "https://www.bax-shop.fr/images/keuzehulp_blokfluit_01.jpg",
-    userId: "1",
-  },
-]);
 </script>
 <template>
   <h1>Hello Brocante</h1>
@@ -112,9 +20,9 @@ const produits = ref([
       <div class="products-grid">
         <!-- Exemple de produit -->
         <CardComponent
-          v-for="produit in produits"
-          v-bind:key="produit.id"
-          v-bind:produit="produit"
+          v-for="article in articles"
+          v-bind:key="article.id"
+          v-bind:article="article"
         />
         <!-- Répétez pour les autres produits -->
       </div>
